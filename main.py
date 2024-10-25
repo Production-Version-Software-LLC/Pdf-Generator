8eb410cffe69aeb561d698af79ab60b1ea77efbe
anl17 <anl.173@outlook.com>
2024-10-25 11:55:28 +0300

    stackTrace 400 bad request

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
-from flask import Flask, request, render_template_string, send_file
+import traceback
+
+from flask import Flask, request, render_template_string, send_file, jsonify
 from weasyprint import HTML
 import io
 
 app = Flask(__name__)
 @app.route('/json-to-pdf', methods=['POST'])
 def json_to_pdf():
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    # html.html dosyasını oku
-    with open('html_template.html', 'r', encoding='utf-8') as dosya:
-        html_template1 = dosya.read()
-
-
-    # JSON parametrelerini işlemek için dinamik HTML şablonu oluşturma
+        # html.html dosyasını oku
+        with open('html_template.html', 'r', encoding='utf-8') as dosya:
+            html_template1 = dosya.read()
 
 
-    # Şablonu JSON verisi ile dolduruyoruz
-    rendered_html = render_template_string(html_template1, **data)
+        # JSON parametrelerini işlemek için dinamik HTML şablonu oluşturma
 
-    # HTML'yi PDF'ye dönüştürüyoruz
-    pdf_file = HTML(string=rendered_html).write_pdf()
 
-    # PDF dosyasını indirilmek üzere döndürüyoruz
-    return send_file(io.BytesIO(pdf_file), download_name='dynamic_report.pdf', as_attachment=True)
+        # Şablonu JSON verisi ile dolduruyoruz
+        rendered_html = render_template_string(html_template1, **data)
 
+        # HTML'yi PDF'ye dönüştürüyoruz
+        pdf_file = HTML(string=rendered_html).write_pdf()
+
+        # PDF dosyasını indirilmek üzere döndürüyoruz
+        return send_file(io.BytesIO(pdf_file), download_name='dynamic_report.pdf', as_attachment=True)
+    except Exception as e:
+        # Capture and log the stack trace
+        stack_trace = traceback.format_exc()
+        app.logger.error("request error %s",request,e,stack_trace)
+
+        # Return a JSON response with the stack trace and error message
+        return jsonify({
+            "error": str(e),
+            "stackTrace": stack_trace
+        }), 400
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
